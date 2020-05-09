8fcd57d9e2b743eda74a6f64079cd22fc263b123
keishi773 <keishi777@hotmail.co.jp>
2020-05-09 21:28:45 +0900

    miss retake AC

--- ABC150B.cpp
+++ ABC150B.cpp
@@ -7,7 +7,7 @@ int main()
     string S;
     cin >> S;
     int count = 0;
-    for (int i = 0; i < N - 3; i++)
+    for (int i = 0; i <= N - 3; i++)
     {
         if (S[i] == 'A' && S[i + 1] == 'B' && S[i + 2] == 'C')
         {
