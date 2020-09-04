11423045b7dc28da6dcf727b558d8439220e8ac4
keishi773 <38573838+keishi773@users.noreply.github.com>
2020-09-04 19:19:37 +0900

    Delete 168B.php

--- 168B.php
+++
@@ -1,9 +0,0 @@
-<?php
-$length = trim(fgets(STDIN));
-$string = trim(fgets(STDIN));
-$cnt = strlen($string);
-if($cnt > $length){
-    $string = substr($string, 0, $length) . "...";
-}
-echo $string;
-?>
