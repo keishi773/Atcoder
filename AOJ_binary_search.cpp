33c5e5bdc58dd0049dcbb35ec5d2ad6da23e0ad2
keishi773 <38573838+keishi773@users.noreply.github.com>
2021-04-27 02:03:14 +0900

    ac

---
+++ AOJ_binary_search.cpp
@@ -0,0 +1,81 @@
+#include <iostream>      // cout, endl, cin
+#include <string>        // string, to_string, stoi
+#include <vector>        // vector
+#include <algorithm>     // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility>       // pair, make_pair
+#include <tuple>         // tuple, make_tuple
+#include <cstdint>       // int64_t, int*_t
+#include <cstdio>        // printf
+#include <map>           // map
+#include <queue>         // queue, priority_queue
+#include <set>           // set
+#include <stack>         // stack
+#include <deque>         // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset>        // bitset
+#include <cmath>
+#include <limits>
+#include <stdio.h>
+#include <string.h>
+#include <numeric>
+#define P pair<int, int>
+#define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const long long INF = 1LL << 60;
+typedef long long ll;
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+// 上下左右の移動用配列
+// セットで上、右、下、左の順番
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+int main(int, char **)
+{
+  int N;
+  cin >> N;
+  vector<int> S(N);
+  rep(i, 0, N) cin >> S[i];
+  int Q;
+  cin >> Q;
+  vector<int> T(Q);
+  rep(i, 0, Q) cin >> T[i];
+
+  //まずTをソートする。
+  sort(T.begin(), T.end());
+  auto itr = lower_bound(S.begin(), S.end(), T[Q - 1]);
+  // cout << *itr << endl;
+  ll cnt = 0;
+  for (int i = 0; i < Q; i++)
+  {
+    if (T[i] <= *itr && find(S.begin(), S.end(), T[i]) != S.end())
+    {
+      cnt++;
+    }
+  }
+  cout << cnt << endl;
+  return 0;
+}
