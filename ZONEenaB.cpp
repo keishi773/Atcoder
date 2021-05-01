4d6aebb93f2ea0851bfaac513f627c2ee46c34d7
keishi773 <38573838+keishi773@users.noreply.github.com>
2021-05-02 08:06:13 +0900

    ac

---
+++ ZONEenaB.cpp
@@ -0,0 +1,79 @@
+// #include <bits/stdc++.h>
+#include <iostream>      // cout, endl, cin
+#include <string>        // string, to_string, stoi
+#include <vector>        // vector
+#include <algorithm>     // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility>       // pair, make_pair
+#include <tuple>         // tuple, make_tuple
+#include <cstdint>       // int64_t, int*_t
+#include <cstdio>        // printf
+#include <map>           // map
+#include <queue>         // queue, priority_queue
+#include <set>           // set
+#include <stack>         // stack
+#include <deque>         // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset>        // bitset
+#include <cmath>
+#include <limits>
+#include <stdio.h>
+#include <string.h>
+#include <numeric>
+#include<iomanip>
+#define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
+#define rep(i, a, b) for (int i = a; i < b; i++)
+const long long INF = 1LL << 60;
+typedef long long ll;
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return true;
+  }
+  return false;
+}
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+// 上下左右の移動用配列
+// セットで上、右、下、左の順番
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+int main(int, char **){
+  // 遮蔽物の数
+  double N;
+  cin >> N;
+
+  // UFOがいる距離
+  double x2;
+  cin >> x2;
+
+  // UFOの高さ
+  double y2;
+  cin >> y2;
+
+
+
+  double ans = 0;
+  for(int i = 0; i < N; i++){
+    double a,b;
+    cin >> a >> b;
+    chmax(ans,b-a*(y2-b)/(x2-a));
+  }
+  cout << ans << endl;
+}
