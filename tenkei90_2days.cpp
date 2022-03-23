4a7d9612abb2812f05a1e4f4f14560c712680203
keishi773 <keishi777@hotmail.co.jp>
2022-03-23 22:05:50 +0900

    tenkei90 2days

--- tenkei90_2days.cpp
+++ tenkei90_2days.cpp
@@ -31,49 +31,55 @@ vector<int> a = { 1, 14, 32, 51, 51, 51, 243, 419, 750, 910 };
 
 // indexが条件を満たすかどうか
 bool isOK(int index, int key) {
-	if (a[index] >= key) return true;
-	else return false;
+    if (a[index] >= key) return true;
+    else return false;
 }
 
 // 二分探索
 int binary_search(int key) {
-	// index0が条件を満たすことがあるので初期値は-1
-	int ng = -1;
-	// index = a.size()-1が条件を満たさないことがある
-	int ok = (int)a.size(); 
-	// okとngのどちらが大きいか分からないことを考慮
-	while (abs(ok - ng) > 1) {
-		int mid = (ok + ng) / 2;
-		if (isOK(mid, key)) ok = mid;
-		else ng = mid;
-	}
-	return ok;
+    // index0が条件を満たすことがあるので初期値は-1
+    int ng = -1;
+    // index = a.size()-1が条件を満たさないことがある
+    int ok = (int)a.size();
+    // okとngのどちらが大きいか分からないことを考慮
+    while (abs(ok - ng) > 1) {
+        int mid = (ok + ng) / 2;
+        if (isOK(mid, key)) ok = mid;
+        else ng = mid;
+    }
+    return ok;
+}
+
+bool hantei(string S) {
+    int cnt = 0;
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i] == '(') cnt += 1;
+        if (S[i] == ')') cnt -= 1;
+        if (cnt < 0) return false;
+    }
+    if (cnt == 0) return true;
+    return false;
 }
 
 // tenkei 2days
 int main() {
-	int N;
+    int N;
     cin >> N;
-    char kleft = '(';
-    char kright = ')';
     // 2^N通り調べて正しければ出力する
-    for(int bit = 0; bit < (1<<N); bit++){
+    for (int bit = 0; bit < (1 << N); bit++) {
         string kakko = "";
-        int leftk = 0;
-        int rightk = 0;
-        bool isOk = false;
-        for(int i = 0; i < N; i++){
-            if(bit & (1<<i)){
-                kakko.push_back(kleft);
-                leftk++;
-            }else{
-                kakko.push_back(kright);
-                rightk++;
+        for (int i = N-1; i >= 0; i--) {
+            if (bit & (1 << i) == 0) {
+                kakko += "(";
+            }
+            else {
+                kakko += ")";
             }
         }
-        if(leftk >= rightk && leftk == rightk){
-            isOk = true;
+        bool isOk = hantei(kakko);
+        if (isOk == true) {
+            cout << kakko << endl;
         }
-        cout << kakko << endl;
     }
+    return 0;
 }
