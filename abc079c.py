7bfc3a65d11656eaff371843580005b549c004c6
keishi773 <38573838+keishi773@users.noreply.github.com>
2022-12-03 22:26:31 +0900

    20221203

---
+++ abc079c.py
@@ -0,0 +1,21 @@
+﻿from collections import deque
+import math
+# 初期値があって便利な連想配列
+from collections import defaultdict
+
+s = input()
+s = list(s)
+str7 = "=7"
+for i in range(1<<3):
+    sum = int(s[0])
+    ans = s[0]
+    for shift in range(3):
+        if i >> shift & 1 == 1:
+            sum += int(s[shift+1])
+            ans += "+" + s[shift+1]
+        else:
+            sum -= int(s[shift+1])
+            ans += "-" + s[shift+1]
+    if sum == 7:
+        print(ans+str7)
+        exit()
---
+++ abc215c.py
@@ -0,0 +1,17 @@
+﻿from collections import deque
+import math
+# 初期値があって便利な連想配列
+from collections import defaultdict
+import itertools
+
+s,k = map(str, input().split())
+k = int(k)
+al = set()
+for seq in itertools.permutations(range(len(s))):
+    sa = ""
+    for i in range(len(s)):
+        sa += s[seq[i]]
+    al.add(sa)
+jisho = list(al)
+jisho.sort()
+print(jisho[k-1])
