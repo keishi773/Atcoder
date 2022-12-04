4a683b260a38be232118043123ce5009b3bcdcaf
keishi773 <keishi777@hotmail.co.jp>
2022-12-04 20:49:51 +0900

    20221204-2

---
+++ abc206c.py
@@ -0,0 +1,29 @@
+﻿from collections import deque
+import math
+# 初期値があって便利な連想配列
+from collections import defaultdict
+import itertools
+
+n = int(input())
+al = list(map(int,input().split()))
+dic = defaultdict(int)
+# 全体の組み合わせ - Ai = Ajの組み合わせ
+zenbu = n * (n - 1) // 2
+# 各文字の個数を出す
+for i in range(n):
+    dic[al[i]] += 1
+ss = 0
+# for i in range(len(dic)):
+#         print("----dec------")
+#         print(dic[i])
+#         print(len(dic))
+#         ss += (dic[i] * (dic[i] - 1)) // 2
+sa = 0
+# for x in dic.values():
+#     sa += x*(x-1)//2
+    # print("----x-----")
+    # print(x)
+# print(zenbu,ss,sa)
+# print(dic.values())
+print(zenbu - sa)
+
---
+++ gdxx.py
@@ -0,0 +1,24 @@
+﻿from collections import deque
+import math
+# 初期値があって便利な連想配列
+from collections import defaultdict
+# 順列/組み合わせを扱う
+import itertools
+# 二分探索
+import bisect
+
+n,sum = map(int, input().split())
+# n = input()
+ans = 0
+# for i in range(1,3000+1):
+#     for j in range(1,3000+1):
+#         for k in range(1,3000+1):
+#             if i + j + k == sum:
+#                 ans += 1
+# print(ans)
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        k = sum - i - j
+        if 1 <= k and k <= n:
+            ans += 1
+print(ans)
